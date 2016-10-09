5b671ef91d3ccb0beb646690cea3a6ed1cf163ba
Jamie Wu <jamie@0587367283.wireless.umich.net>
2016-10-09 15:47:57 -0400

    add donot split reads scheduler

---
+++ NoSplitReadsScheduler.cpp
@@ -0,0 +1,137 @@
+//
+// Created by Jamie Wu on 10/9/16.
+//
+
+#include "NoSplitReadsScheduler.h"
+#include "object.h"
+#include "simulator.h"
+#include <algorithm>
+
+NoSplitReadsScheduler::NoSplitReadsScheduler() : Scheduler() {}
+
+NoSplitReadsScheduler::NoSplitReadsScheduler(Simulator* sim) : sim(sim) {}
+
+void NoSplitReadsScheduler::init()
+{
+    exclTrans.resize(sim->getTotalObj());
+    inclTrans.resize(sim->getTotalObj());
+    sizeO.resize(sim->getTotalObj(), 0);
+    sizeT.resize(sim->getTotalTrans());
+    for(int i = 0; i < sizeT.size(); i ++){
+        sizeT[i].first = i;
+        sizeT[i].second = 0;
+    }
+}
+
+bool NoSplitReadsScheduler::acquire(int tid, int oid, bool excl)
+{
+    int status = sim->getObj(oid).getStatus();
+
+    if (status == Object::FREE) {
+        updateT(tid, sizeO[oid]);
+        std::set<int> trans;
+        trans.insert(tid);
+        sim->getObj(oid).addOwner(trans, excl);
+        return true;
+    }
+
+    if (excl) {
+        exclTrans[oid].push_back(&sizeT[tid]);
+        updateO(oid, 1 + sizeT[tid].second);
+    }
+    else {
+        inclTrans[oid].push_back(tid);
+        updateO(oid, 1 + sizeT[tid].second);
+    }
+
+    return false;
+}
+
+void NoSplitReadsScheduler::release(int tid, int oid) {
+    sim->getObj(oid).releaseBy(tid);
+    updateT(tid, -sizeO[oid]);
+
+    if (sim->getObj(oid).getStatus() == Object::FREE) {
+        if (!exclTrans[oid].empty() || !inclTrans[oid].empty()) {
+            sim->addToAssign(oid);
+        }
+    }
+}
+
+const std::set<int> NoSplitReadsScheduler::assign(int oid)
+{
+    std::set<int> assigned;
+
+    std::sort(exclTrans[oid].begin(), exclTrans[oid].end(), myComparater);
+
+    int inclSize = 0;
+    for (auto itr = inclTrans[oid].begin(); itr != inclTrans[oid].end(); ++itr)
+    {
+        inclSize += sizeT[*itr].second + 1;
+    }
+
+    int n, min_position = exclTrans.size();
+
+    double latency, min_latency = n * inclSize;
+
+    double readTime = f(inclSize);
+    while(n > 0) {
+        latency += (- inclSize + readTime * exclTrans[oid][n-1]->second);
+        if (min_latency > latency) {
+            min_latency = latency;
+            min_position --;
+        }
+        n --;
+    }
+
+
+    if (min_position > 0) { //assign the lock to write
+        int trans = exclTrans[oid][0]->second;
+
+        updateO(oid, - exclTrans[oid][0]->second - 1);
+
+        sim->getTrans(trans).grantLock();
+
+        updateT(trans, sizeO[oid]); // grant lock BEFORE this update ! ! !
+
+        assigned.insert(trans);
+        exclTrans[oid].erase(exclTrans[oid].begin());
+
+        sim->getObj(oid).addOwner(assigned, true);
+    }
+    else {  //assign the lock to reads
+        updateO(oid, - inclSize);
+        for (auto itr = inclTrans[oid].begin(); itr != inclTrans[oid].end(); ++itr)
+        {
+            sim->getTrans(*itr).grantLock();
+            updateT(*itr, sizeO[oid]); // grant lock BEFORE this update ! ! !
+            assigned.insert(*itr);
+        }
+
+        inclTrans[oid].clear();
+
+        sim->getObj(oid).addOwner(assigned, false);
+    }
+
+    return assigned;
+}
+
+int NoSplitReadsScheduler::getTime() { return sim->getTime(); }
+
+void NoSplitReadsScheduler::updateO(int oid, int delta)
+{
+    sizeO[oid] += delta;
+    for (auto itr = sim->getObj(oid).getOwner().begin(); itr != sim->getObj(oid).getOwner().end(); ++itr)
+    {
+        updateT(*itr, delta);
+    }
+}
+
+void NoSplitReadsScheduler::updateT(int tid, int delta) {
+    sizeT[tid].second += delta;
+    int oid = sim->getTrans(tid).getBlockBy();
+    if (oid > 0)
+    {
+        updateO(oid, delta);
+    }
+}
---
+++ NoSplitReadsScheduler.h
@@ -0,0 +1,55 @@
+//
+// Created by Jamie Wu on 10/9/16.
+//
+
+#ifndef SCHEDULER_NOSPLITREADSSCHEDULER_H
+#define SCHEDULER_NOSPLITREADSSCHEDULER_H
+
+#include <vector>
+#include <queue>
+#include "scheduler.h"
+#include "transaction.h"
+#include <utility>
+#include <math.h>
+
+using namespace std;
+class Simulator;
+
+
+class NoSplitReadsScheduler : public Scheduler
+{
+private:
+    struct Comparater { // customized comparater for the priority queue
+        bool operator() (pair<int, int>* Ta, pair<int, int>* Tb)
+        {
+            if (Ta->second < Tb->second)
+                return true;
+            else
+                return false;
+        }
+    } myComparater;
+    vector<int> sizeO;
+    vector< pair<int, int> > sizeT;
+    vector< vector< pair<int, int>*> > exclTrans;
+    vector<std::vector<int> > inclTrans;
+    Simulator* sim;
+
+
+
+private:
+    double f(int r) { return log(1.0 + r) / log(2.);}
+    void updateO(int oid, int delta);
+    void updateT(int tid, int delta);
+
+public:
+    NoSplitReadsScheduler();
+    NoSplitReadsScheduler(Simulator* sim);
+    void init();
+    bool acquire(int tid, int oid, bool excl);
+    void release(int tid, int oid);
+    const std::set<int> assign(int oid);
+    int getTime();
+};
+
+
+#endif //SCHEDULER_NOSPLITREADSSCHEDULER_H
