da4ae0f6c957c80bd350607caefbeb2e59036b6c
Jamie Wu <jamie@0587380410.wireless.umich.net>
2016-10-12 15:29:21 -0400

    update gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -3,51 +3,53 @@
 #include <cstdlib>
 #include <vector>
 #include "action.h"
+#include "RandomSeqGen.cpp"
+
 using namespace std;
 
 const int NUMLOCK = 10;
 const int NUMTRAN = 20000;
+const int NUM_TRAIL = 10;
 
-const double NEWT = 0.1;
-const double NEWA = 0.05;
-const double FINPROB = 0.2;
-const double LOCK_TYPE_PROB = 0.5;
+const double NEWT = 0.032;
+const double NEWA = 0.005;
+
+const double LOCK_TYPE_PROB = 0.75;
+
+const bool USE_POWER_LAW = true;
+
+const double POWER_LAW_PARAM = 1.5;
+//const double FINPROB = 0.35;
 
 default_random_engine genT((unsigned) time(0));
 exponential_distribution<double> poiT(NEWT);
 default_random_engine genA((unsigned) time(0));
 exponential_distribution<double> poiA(NEWA);
 
-vector<Action> genTrans()
+vector<Action> genTrans(RandomSeqGen* randGen)
 {
     int time = 0;
     vector<Action> act;
     act.push_back(Action(time, Action::START, true));
-    int lockFrom = 0;
-    while (true)
-    {
+
+    set<int> objSeq;
+
+    if (USE_POWER_LAW){
+        objSeq = randGen->getPowerLawSeq(NUM_TRAIL);
+    } else {
+        objSeq = randGen->getUniformSeq(NUM_TRAIL);
+    }
+
+    for(auto& obj: objSeq){
         int dtime = 1 + poiA(genA);
 
-        
-        double r = rand() / (double) RAND_MAX;
         double t = rand() / (double) RAND_MAX;
         bool lock_type = t <= LOCK_TYPE_PROB;
 
-        if (lockFrom == NUMLOCK) r = 0.;
-
-        if (r < FINPROB) // finish
-        {
-            act.push_back(Action(time += dtime, Action::FINISH, true));
-            break;
-        }
-        else // acquire a lock 
-        {
-            // only locks from lockFrom can be acquired
-            int lock = rand() % (NUMLOCK - lockFrom) + lockFrom; // each lock has equal probability to be acquired
-            act.push_back(Action(time += dtime, lock, lock_type));
-            lockFrom = lock + 1;
-        }
+        act.push_back(Action(time += dtime, obj, lock_type));
     }
+    int dtime = 1 + poiA(genA);
+    act.push_back(Action(time += dtime, Action::FINISH, true));
 
     return act;
 }
@@ -55,11 +57,11 @@ vector<Action> genTrans()
 int main()
 {
     cout << NUMLOCK << ' ' << NUMTRAN << endl;
-
+    RandomSeqGen* randGen = new RandomSeqGen(NUMLOCK, POWER_LAW_PARAM);
     int startTime = 0;
     for (int i = 0; i < NUMTRAN; ++i)
     {
-        vector<Action> acts = genTrans();
+        vector<Action> acts = genTrans(randGen);
         cout << startTime << ' ' << acts.size() << endl;
         for (int j = 0; j < acts.size(); ++j)
         {
