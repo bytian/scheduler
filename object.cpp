15f96f3a9943170c9c2dc64f74392f05f32b1b3d
bytian <bytian@umich.edu>
2016-10-18 19:14:15 -0400

    add assertion

--- object.cpp
+++ object.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <set>
+#include <assert.h>
 
 int Object::getStatus() const
 {
@@ -14,6 +15,20 @@ int Object::getStatus() const
 
 void Object::addOwner(const std::set<int>& tid, bool excl)
 {
+    if (getStatus() == EXCL)
+    {
+        std::cerr << "Object #" << id << " is held by transaction " << *(owner.begin()) << " exclusively." << std::endl;
+        std::cerr << "Cannot add more owner." << std::endl;
+        std::cout << "Assertion failure." << std::endl;
+        assert(false);
+    }
+    if (getStatus() == INCL && excl)
+    {
+        std::cerr << "Object #" << id << "is protected by an inclusive lock" << std::endl;
+        std::cerr << "Cannot grant an exclusive lock on it." << std::endl;
+        assert(false);
+        std::cout << "Assertion failure." << std::endl;
+    }
     for (auto itr = tid.begin(); itr != tid.end(); ++itr)
     {
         owner.insert(*itr);
